c2cb5360f87f07dfa3dca6cce9d4249fa13dce2e
Joseph Terribile <kartingman25@gmail.com>
2021-12-18 17:45:10 -0500

    Add prettier for auto formatting

--- src/app-renderer.tsx
+++ src/app-renderer.tsx
@@ -2,8 +2,8 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
 ReactDOM.render(
-    <React.StrictMode>
-        <h1>Hello from react</h1>
-    </React.StrictMode>,
-    document.body
+  <React.StrictMode>
+    <h1>Hello from react</h1>
+  </React.StrictMode>,
+  document.body
 );
