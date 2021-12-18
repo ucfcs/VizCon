ca1b7c8735a5c978b4ce796bf0dee695c8db37b8
Joseph Terribile <kartingman25@gmail.com>
2021-12-18 17:24:37 -0500

    Add react to project and add base renderer

---
+++ src/app-renderer.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <React.StrictMode>
+        <h1>Hello from react</h1>
+    </React.StrictMode>,
+    document.body
+);
