a6e5da8233c1565f27e242f00f161ce1b1ee21f3
Joseph Terribile <kartingman25@gmail.com>
2022-01-13 21:47:59 -0500

    Style up the nav bar for windows and linux

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import WindowControls from './nav/windowcontrols';
+import MenuItem from './nav/menuitem';
 import '../styles/nav.scss';
 
 export default function Nav(): React.ReactElement {
   const showMenu = window.platform.getPlatform() !== 'darwin';
 
   return (
-    <div className="titlebar">
+    <div className="titlebar" style={{height: '30px'}}>
       {showMenu && (
         <>
+          <div className='titlebar-drag-region'></div>
           <a className="appicon"></a>
-          <div className="menubar" role="menubar"></div>
+          <div className="menubar" role="menubar">
+            <MenuItem />
+          </div>
         </>
       )}
       {/* TODO: have this title be dynamic, and when it updates, update window.title */}
