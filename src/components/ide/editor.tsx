5c3c084feab09b7d2a3b63bfd3ee5d3c70b416dc
Joseph Terribile <kartingman25@gmail.com>
2022-01-14 23:26:58 -0500

    Add first monaco instance

--- src/components/ide/editor.tsx
+++ src/components/ide/editor.tsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import * as monaco from 'monaco-editor';
 
 interface EditorProps {
   file: string;
 }
 
 export default function Editor({ file }: EditorProps): React.ReactElement {
-  return <div>{file}</div>;
+  const div = useRef<HTMLDivElement>(null);
+  const [monacoInstance, setMonacoInstance] = useState<monaco.editor.IStandaloneCodeEditor>(null);
+
+  useEffect(() => {
+    if (div.current) {
+      // TODO: file reading
+      // TODO: allow unique monaco instances (this doesnt work right now)
+      const editor = monaco.editor.create(div.current, {
+        value: ['int main(void) {', '    return 0;', '}'].join('\n'),
+        language: 'c',
+        automaticLayout: true
+      });
+      setMonacoInstance(editor);
+    }
+
+    return () => {
+      monacoInstance.dispose();
+    }
+  }, []);
+
+  return <div className='editor' ref={div}></div>;
 }
