9f3f4deb6cf65d30af15f6ab1f4d4889448a493c
Joseph Terribile <kartingman25@gmail.com>
2022-01-27 21:37:41 -0500

    Update EditorOld

--- src/components/ide/editorold.tsx
+++ src/components/ide/editorold.tsx
@@ -1,5 +1,6 @@
 // this file is currently just a save of the old way of dong the editor component, for if it is ever needed again
 // DONT USE IT
+// Scratch that, this is the one we are going to use, as it allows us to have more granular control
 import React, { useEffect, useRef, useState } from 'react';
 import * as monaco from 'monaco-editor';
 
@@ -9,34 +10,46 @@ interface EditorProps {
 
 export default function EditorOld({ file }: EditorProps): React.ReactElement {
   const div = useRef<HTMLDivElement>(null);
+  // TODO: better default content
+  const [fileContent, setFileContent] = useState<string>(['int main(void) {', '    return 0;', '}', '// ' + file].join('\n'));
   const [monacoInstance, setMonacoInstance] = useState<monaco.editor.IStandaloneCodeEditor>(null);
 
+  // this use effect is pureley here for the initial mount of the component
+  // this *will* need to be done better, because there is no way to dispose the monaco editor
+  // to do that, we *might* need to do some hacky call backs and what not to propigate the dispose call
+  // to the tab closing
   useEffect(() => {
-    async function run() {
-      console.log(file);
-      if (div.current) {
-        let fileContent = await window.platform.readFileSync(file);
-        console.log(fileContent);
-        if (fileContent.length == 0 || fileContent.startsWith('ERROR:')) {
-          fileContent = ['int main(void) {', '    return 0;', '}', '// ' + file].join('\n');
-        }
-
-        // TODO: allow unique monaco instances (this doesnt work right now)
-        const editor = monaco.editor.create(div.current, {
-          value: fileContent,
-          language: 'c',
-          automaticLayout: true,
-          theme: 'vs-dark',
-        });
-        setMonacoInstance(editor);
-      }
+    console.log(monacoInstance)
+    if (div.current && !monacoInstance) {
+      const editor = monaco.editor.create(div.current, {
+        value: fileContent,
+        language: 'c',
+        automaticLayout: true,
+        theme: 'vs-dark',
+      });
+      setMonacoInstance(editor);
     }
-    run();
-
-    return () => {
-      monacoInstance.dispose();
-    };
   }, []);
+
+  useEffect(() => {
+    run();
+    async function run() {
+      // only run if monaco has been loaded
+      if (!monacoInstance) return;
+      const fileContent = await window.platform.readFileSync(file);
+      console.log(fileContent);
+      if (fileContent?.length == 0 || fileContent.startsWith('ERROR:')) {
+        // TODO: open modal here and prevent loading of the entire component
+        // This might be better acomplished by changing the call to just read file,
+        // and instead making the open modal happen somewhere higher up the FC call stack
+        // Did that, it is just a read call now
+        console.error('Bad file or path', file, fileContent);
+        return;
+      }
+      setFileContent(fileContent);
+      monacoInstance.setValue(fileContent);
+    }
+  }, [file, monacoInstance]);
 
   return <div id={file} className="editor-container" ref={div}></div>;
 }
