a789f899efc10668c64588be32e6e7b1ea2af326
Marco Portocarrero <77308615+Effectualness@users.noreply.github.com>
2022-02-08 19:01:26 -0500

    semaphore structure and utils update
    -Added name and number fields to semaphore structure
    -Added functions to create names to utils library
    -Changed vizconError to take char string in first argument instead of number, for easier use and consistency of this function

--- concurrency/utils.h
+++ concurrency/utils.h
@@ -1,17 +1,19 @@
 #ifndef UTILS_H
 #define UTILS_H
+
 //universal libraries
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #ifdef _WIN32 // window's libraries and definitions
-#include <windows.h>
+    #include <windows.h>
 #elif __linux__ || __APPLE__ //Linux and MacOS's libraries and definitions
-#include <errno.h>
-#include <unistd.h>
+    #include <pthread.h>
+    #include <errno.h>
 #endif
 
-//Functions
-void vizconError(int func, int err);
+//Other functions
+char* vizconCreateName(int type, int value);
+int vizconStringLength(char* name);
+void vizconError(char* func, int err);
 
 #endif
