a053ce4686f48e0b23462bfe1eb49be8e3af61eb
Carlson Sharpless <cwsharpless@gmail.com>
2022-02-25 10:12:35 -0500

    Rearranged error codes

--- concurrency/utils.h
+++ concurrency/utils.h
@@ -23,15 +23,20 @@
 #define VC_TYPE_SEM 1
 #define VC_TYPE_MUTEX 2
 
-// VizCon error codes
-#define VC_ERROR_ABANDONED 500
-#define VC_ERROR_TIMEOUT 501
-#define VC_ERROR_MEMORY 502
-#define VC_ERROR_BADCOUNT 503
-#define VC_ERROR_NAMEERROR 504
+// VizCon error codes.
+// 50X - Error with object creation
+#define VC_ERROR_MEMORY 500
+#define VC_ERROR_NAMEERROR 501
+#define VC_ERROR_BADCOUNT 502
+
+// 51X - Error with mutex permissions
 #define VC_ERROR_DOUBLEUNLOCK 510
 #define VC_ERROR_DOUBLELOCK 511
 #define VC_ERROR_CROSSTHREADUNLOCK 512
+
+// 52X - Unexpected system response (Windows only)
+#define VC_ERROR_ABANDONED 520
+#define VC_ERROR_TIMEOUT 521
 
 // Other constants
 #define MAX_ERROR_MESSAGE_LENGTH 200
