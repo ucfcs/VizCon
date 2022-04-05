9cef4a2ce60affb2a497df9d018a30299e26061d
Carlson Sharpless <cwsharpless@gmail.com>
2022-04-05 18:55:43 -0400

    Adjusted test files for high-speed testing

--- ide_testing/infrastructure/pause.c
+++ ide_testing/infrastructure/pause.c
@@ -2,15 +2,19 @@
 
 int main()
 {
+    // Wait 1 second.
+    // This ensures the program doesn't start before console checks are complete.
+    vcThreadSleep(1000);
+
     printf("Before");
     
     // Wait 2 seconds.
-    #ifdef _WIN32
-    Sleep(2000);
-    #else
-    sleep(2);
-    #endif
+    vcThreadSleep(2000);
 
     printf("After");
+
+    // Wait 1 second.
+    // This ensures the program doesn't finish before button checks are complete.
+    vcThreadSleep(1000);
     return 0;
 }
--- ide_testing/infrastructure/run.c
+++ ide_testing/infrastructure/run.c
@@ -2,6 +2,10 @@
 
 int main()
 {
+    // Wait 1 second.
+    // This ensures the program doesn't finish before button checks are complete.
+    vcThreadSleep(1000);
+
     printf("Hello, World!");
     return 0;
 }
