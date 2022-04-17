92c8bd45bdc9727f86ab54fcdf6e8486cf2b8193
Marco Portocarrero <77308615+Effectualness@users.noreply.github.com>
2022-04-16 20:23:03 -0400

    Revert to allow semaphore/mutex creation in thread
    -Previously disallowed, but functions correctly. Only thread creation will be prevented from threads
    -fixed implicit declaration warning from vcuserlibrary

--- concurrency/utils.c
+++ concurrency/utils.c
@@ -88,12 +88,12 @@ void vizconError(char* func, int err)
             }
             case VC_ERROR_CREATEDISABLED:
             {
-                errorMessage = "Threads, semaphores, and mutexes may not be created while threads are active.";
+                errorMessage = "Threads may not be queued while threads are active.";
                 break;
             }
             case VC_ERROR_THREADSACTIVE:
             {
-                errorMessage = "An instance of this call is already active.";
+                errorMessage = "An instance of a thread starting function is already active.";
                 break;
             }
             default:
